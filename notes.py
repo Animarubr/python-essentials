5007dca7f1b964cf0f94904bd07a3ad129935ea9
Marcos <marvisilva202@gmail.com>
2023-08-31 10:37:04 -0300

    Error: LBYL + race condition

---
+++ notes.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Bloco de notas
+
+$ notes.py new "Minha Nota"
+tag: tech
+text: 
+Anotacao geral sobre carreira de tecnologia
+
+$ notes.py read tech
+...
+...
+"""
+__version__ = "0.1.0"
+
+import os
+import sys
+
+path = os.curdir
+filepath = os.path.join(path, "notes.txt")
+cmds = ("read", "new")
+
+arguments = sys.argv[1:]
+if not arguments:
+    print("Invalid usage")
+    print(f"You must specify subcommand: {cmds}")
+    sys.exit(1)
+
+if arguments[0] not in cmds:
+    print(f"Invalid command {arguments[0]}")
+
+if arguments[0] == "read":
+    for line in open(filepath):
+        title, tag, text = line.split("\t")
+        if tag.lower() == arguments[1].lower():
+            print(f"Title: {title}")
+            print(f"Text: {text}")
+            print("-" * 30)
+            print()
+
+if arguments[0] == "new":
+    title = arguments[1]
+    text = [
+        f"{title}",
+        input("tag:").strip(),
+        input("text:\n").strip(),
+    ]
+    # \t - tsv
+    with open(filepath, "a") as file_:
+        file_.write("\t".join(text) + "\n")
