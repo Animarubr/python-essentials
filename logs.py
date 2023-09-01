d7b5b6a998bd67f6f629e97dc7ec5c7090336169
Marcos <marvisilva202@gmail.com>
2023-09-01 10:15:47 -0300

    add handlers

--- logs.py
+++ logs.py
@@ -2,7 +2,7 @@
 
 import os
 import logging
-
+from logging import handlers
 
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
@@ -11,23 +11,26 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 # TODO: usar lib (loguru)
 log = logging.Logger(__name__, log_level)
 # level
-ch = logging.StreamHandler()  # Console/terminal/stderr
-ch.setLevel(log_level)
+# ch = logging.StreamHandler()  # Console/terminal/stderr
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler("meulog.log", maxBytes=100, backupCount=10)
+fh.setLevel(log_level)
 # formatação
 fmt = logging.Formatter(
     "%(asctime)s %(name)s %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s"
 )
-ch.setFormatter(fmt)
+# ch.setFormatter(fmt)
+fh.setFormatter(fmt)
+
 # destino
-log.addHandler(ch)
+# log.addHandler(ch)
+log.addHandler(fh)
 
 """log.debug("Mensagem para o dev, qe, sysadmin")
 log.info("Mensagem para os usuários")
 log.warning("Aviso que não causa erro")
 log.error("Erro que afeta uma unica execução")
 log.critical("Erro geral ex: Banco de dados sumiu")"""
-
-print("---")
 
 try:
     1 / 0
